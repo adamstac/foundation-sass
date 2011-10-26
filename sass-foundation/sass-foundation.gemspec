e52dedf4102daf77d457e05cbeb0aea9b91c32bf
Chris <chris@zurb.com>
2011-10-26 11:05:23 -0700

    added files over

---
+++ sass-foundation/sass-foundation.gemspec
@@ -0,0 +1,25 @@
+Gem::Specification.new do |s|
+  # Release Specific Information
+  s.version = "2.0.1"
+  s.date = "2011-10-26"
+
+  # Gem Details
+  s.name = "sass-foundation"
+  s.authors = ["ZURB"]
+  s.summary = %q{ZURB Foundation all Sassyfied}
+  s.description = %q{ZURB Foundation all Sassyfied for quickly jumping into foundation. }
+  s.email = "foundation@zurb.com"
+  s.homepage = "http://foundation.zurb.com"
+
+  # Gem Files
+  s.files = %w(README.mkdn)
+  s.files += Dir.glob("lib/**/*.*")
+  s.files += Dir.glob("stylesheets/**/*.*")
+  s.files += Dir.glob("templates/**/*.*")
+
+  # Gem Bookkeeping
+  s.has_rdoc = false
+  s.require_paths = ["lib"]
+  s.rubygems_version = %q{1.3.6}
+  s.add_dependency("compass", [">= 0.10.0.rc3"])
+end
---
+++ sass-foundation/templates/project/manifest.rb
@@ -0,0 +1,74 @@
+# Make sure you list all the project template files here in the manifest.
+file "index.html"
+file "humans.txt"
+file "robots.txt"
+
+# Config File needs --force to work when creating a project
+file "config.rb"
+
+# CSS Files
+file "stylesheets/app.css", :media => 'screen'
+file "stylesheets/forms.css", :media => 'screen'
+file "stylesheets/globals.css", :media => 'screen'
+file "stylesheets/ie.css", :media => 'screen'
+file "stylesheets/orbit.css", :media => 'screen'
+file "stylesheets/reveal.css", :media => 'screen'
+file "stylesheets/ui.css", :media => 'screen'
+file "stylesheets/mobile.css", :media => 'screen'
+file "stylesheets/grid.css", :media => 'screen'
+file "stylesheets/typography.css", :media => 'screen'
+
+# Image Files
+file "images/misc/button-gloss.png"
+file "images/misc/button-overlay.png"
+file "images/misc/custom-form-sprites.png"
+file "images/misc/input-bg.png"
+file "images/misc/modal-gloss.png"
+file "images/misc/table-sorter.png"
+file "images/orbit/bullets.jpg"
+file "images/orbit/left-arrow.png"
+file "images/orbit/loading.gif"
+file "images/orbit/mask-black.png"
+file "images/orbit/pause-black.png"
+file "images/orbit/right-arrow.png"
+file "images/orbit/rotator-black.png"
+file "images/orbit/timer-black.png"
+
+# Javascrips
+file "javascripts/app.js"
+file "javascripts/forms.jquery.js"
+file "javascripts/jquery.customforms.js"
+file "javascripts/jquery-1.6.4.min.js" 
+file "javascripts/jquery.reveal.js"
+file "javascripts/jquery.orbit-1.3.0.js"
+file "javascripts/jquery.placeholder.min.js"
+
+# Sass Files
+file "sass/app.sass"
+file "sass/forms.sass"
+file "sass/globals.sass"
+file "sass/ie.sass"
+file "sass/orbit.sass"
+file "sass/reveal.sass"
+file "sass/ui.sass"
+file "sass/mobile.sass"
+file "sass/grid.sass"
+file "sass/typography.sass"
+#Sass Mixin Partials
+file "sass/includes/_mixins.sass"
+file "sass/includes/_settings.sass"
+
+
+
+description "sass-foundation"
+
+help %Q{
+	
+		
+}
+
+welcome_message %Q{
+	
+	sass-foundation
+	
+}
