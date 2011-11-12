801f35c03e645fd0d37325d10eb0e4b99d79bd8b
Adam Stacoviak <adam@stacoviak.com>
2011-11-11 21:14:52 -0600

    Opps. Droped some dup code. My bad :)

--- lib/foundation-sass.rb
+++ lib/foundation-sass.rb
@@ -1,8 +1,4 @@
 require 'compass'
-extension_path = File.expand_path(File.join(File.dirname(__FILE__), ".."))
-Compass::Frameworks.register('foundation-sass', :path => extension_path)
-
-require 'compass'
 Compass::Frameworks.register("foundation-sass", :path => "#{File.dirname(__FILE__)}/..")
 
 module FoundationSass
